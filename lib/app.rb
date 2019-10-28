7a4e6cab3c63bf8318ca3db4d638b6006dd417e9
Tenzin <tenzinball@icloud.com>
2019-10-28 15:03:45 +0000

    added views

--- lib/app.rb
+++ lib/app.rb
@@ -1,7 +1,7 @@
 require 'sinatra'
 require 'shotgun'
 
-  set :session_secret, 'super secret!'
+set :session_secret, 'super secret!'
 
 get '/' do
   'Hello Tenzin and Natalie!'
@@ -12,7 +12,5 @@ get '/secret' do
 end
 
 get '/cat' do
-  "<div style='border: 3px dashed red'>
-  <img src= 'http://bit.ly/1eze8aE'>
-  </div>"
+  erb(:index)
 end
